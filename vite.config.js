7b790b6525ff2dd04d96fbc5fd996106c2917e34
Ahtesham Quraish <ahtesham.quraish@A006-01455.local>
2025-05-21 12:48:38 +0500

    Add GitHub Actions workflow 13

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/vite/',  // 👈 This must match your repo name
+  plugins: [react(), eslint()],
+  base: '/vite/', // 👈 This must match your repo name
   test: {
     globals: true,
     environment: 'jsdom',
   },
-})
+});
